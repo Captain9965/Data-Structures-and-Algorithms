7a0df7798116d93fe59395ea39ff45d9ad912afc
lenny <lennyweda@gmail.com>
2022-11-29 06:27:32 +0300

    (f/tree): B-tree insertion cpp implementation complete

--- Data Structures/Tree-based-DSAsII/insert-b-tree.cpp
+++ Data Structures/Tree-based-DSAsII/insert-b-tree.cpp
@@ -57,12 +57,12 @@ Node::Node(int t1, bool leaf1){
 void Node::traverse(){
     int i;
     for (i = 0; i < n ; i ++){
-        if (leaf == false){
+        if (!leaf){
             C[i]->traverse();
         }
         cout <<" " << keys[i];
     }
-    if (leaf == false){
+    if (!leaf){
         C[i]->traverse();
     }
 }
@@ -83,11 +83,85 @@ void BTree::insert(int k){
             int i = 0;
             if (s->keys[0] < k){
                 i++;
-                s->C[i]->insertNonFull(k);
-                root = s;
-            } else{
+            }
+            s->C[i]->insertNonFull(k);
+            root = s;
+             }else{
                 root->insertNonFull(k);
             }
+    }
+}
+
+/* insert non-full condition: */
+
+void Node::insertNonFull(int k){
+    int i = n - 1;
+
+    if (leaf){
+        while (i <= 0 && keys[i] > k){
+            keys[i + 1] = keys[i];
+            i --; 
+        }
+        keys[i + 1] = k;
+        n = n + 1;
+    } else{
+        while ( i <= 0 && keys[i] > k){
+            i --;
+        }
+            
+            if (C[i + 1]->n == 2 * t - 1){
+                splitChild(i + 1, C[i + 1]);
+
+                if (keys[i + 1] < k){
+                    i ++;
+                }
+            }
+            C[i + 1]->insertNonFull(k);
+    }
+}
+/* split the child: */
+
+void Node::splitChild(int i, Node * y){
+    Node *z = new Node(y->t, y->leaf);
+    z->n = t - 1;
+
+    for (int j = 0; j < t - 1; j ++ ){
+        z->keys[j] = y->keys[j + t];
+    }
+    if (!y->leaf){
+        for(int j = 0; j < t; j ++){
+            z->C[j] = y->C[j + t];
         }
     }
+
+    y->n = t - 1;
+    for (int j = n; j >= i + 1;  j--){
+        C[j + 1] = C[j];
+    }
+    
+    C[i + 1] = z;
+
+    for(int j = n - 1; j >= i; j--){
+        keys[j + 1] = keys[j];
+    }
+
+    keys[i] = y->keys[t -1];
+    n = n + 1;
+}
+
+int main(){
+    BTree t(3);
+    t.insert(8);
+    t.insert(9);
+    t.insert(10);
+    t.insert(11);
+    t.insert(15);
+    t.insert(16);
+    t.insert(17);
+    t.insert(18);
+    t.insert(20);
+    t.insert(23);
+
+    cout << "The tree is -> ";
+    t.traverse();
 }
