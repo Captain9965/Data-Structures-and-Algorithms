8f0fcb8ce94bb6af8457f7e12d76ec6052b433d4
lenny <lennyweda@gmail.com>
2023-02-24 15:05:47 +0300

    (f/array): array question done and optimized for O(n) time and space complexity

--- Data Structures/Data-structures-1/arrays/array-question.py
+++ Data Structures/Data-structures-1/arrays/array-question.py
@@ -26,18 +26,74 @@
     Logically, we can easily add each number by adding each number with each other number..this is the 2 pointer technique
 
     step 4: write the solution in code: 
+    step 5: Double check for errors
+    step 6: Test solution with test cases.
+    step 7: analyze the space and time complexity of the solution.
+    step 8: optimizing the solution.
 
  
 
  """
 
-def returnTwoPairs(array):
+ #-------------------------------------------------------------------------------------------------#
+"""
+    This is the brute force solution: 
+
+    Time complexity for this algorithm is O(n2)
+    space complexity for this is O(1)
+"""
+def returnTwoPairs(array, num):
+
+    # if the array is empty or contains only one item, then return null or None in python
+    if len(array) <= 1:
+        return None
     for p1 in range(len(array)):
         for p2 in range(len(array) - (p1 + 1)):
-            print(array[p1], end= " ")
-            print(array[p1 + 1 + p2])
+            index1 = p1
+            index2 = p1 + 1 + p2
+
+            if (array[index1] + array[index2] == num):
+                return [index1, index2]
+    #if we do not find any pair, return None
+    return None
+
+
+"""
+    we can use a hash map or a hash map like data structure to store the number we need to look for in the subsequent elements.
+    This way, we do not need to use a second loop just to do the comparison:
+
+    time complexity is -> O(n)
+    space complexity is -> O(n)
+
+
+"""
+
+def hashMap_solution(array, num):
+    # if the array is empty or contains only one item, then return null or None in python
+    if len(array) <= 1:
+        return None
+        
+    solutionMap = {}
+    for index1 in range(len(array)):
+        index2 = solutionMap.get(array[index1])
+    
+        if index2 is not None:
+            return [index2, index1]
+        else:
+            solutionMap[num - array[index1]] = index1
+    return None
 
 
 if __name__ == "__main__":
     array = [1, 3, 7, 9, 2]
-    returnTwoPairs(array)
+    array2 = []
+    array3 = [1, 4, 34, 4, 5]
+    array4 = [11]
+    array5 = [9, 2]
+
+    result = hashMap_solution(array, 11)
+    if (result is not None):
+        print(result)
+    else:
+        print("No result")
+
