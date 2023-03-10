392a3fa11a0f848c3f3159dc27893aeb5e8d1d5d
lenny <lennyweda@gmail.com>
2023-03-10 06:53:44 +0300

    (f/stack_and_queue_questions): implement queue with stacks done

--- Data Structures/Data-structures-1/Queue/queue/queue_with_stacks.py
+++ Data Structures/Data-structures-1/Queue/queue/queue_with_stacks.py
@@ -4,30 +4,73 @@
 
 """
 
+
+
 class MyQueue:
 
     def __init__(self):
         self.stack = []
     def push(self, x: int) -> None:
         self.stack.append(x)
-        self.stack = self.stack.reverse()
-
     def pop(self) -> int:
         if not self.empty():
-            return self.stack.pop()
+            self.stack.reverse()
+            value = self.stack.pop()
+            self.stack.reverse()
+            return value
         else:
-            return -1
+            return None
 
     def peek(self) -> int:
         if not self.empty():
-            return self.stack[len(self.stack) - 1]
+            return self.stack[0]
         else:
-            return -1
+            return None
 
     def empty(self) -> bool:
         return len(self.stack) < 1
 
+class MyQueueWithStacks:
+    """ Queue takes up O(n) space"""
+    def __init__(self):
+        self.In = []
+        self.Out = []
+
+    """ O(1) for both time and space """
+    def push(self, x :int):
+        self.In.append(x)
+
+    """ Time is O(n) and space is O(1)"""
+    def pop(self):
+        if len(self.Out):
+            return self.Out.pop()
+        if len(self.In):
+            while len(self.In):
+                self.Out.append(self.In.pop())
+            return self.Out.pop()
+        else:
+            return None
+        
+    """ Time is O(n) and space is O(1)"""
+    def peek(self):
+        if not self.empty():
+            if len(self.Out):
+                return self.Out[len(self.Out) - 1]
+            if len(self.In):
+                while len(self.In):
+                    self.Out.append(self.In.pop())
+                return self.Out[len(self.Out) - 1]
+        else:
+            return None
+        
+    """ Time is O(1)"""
+    def empty(self):
+        return len(self.In) < 1 and len(self.Out) < 1
+            
+
+
+
 if __name__ == "__main__":
-    queue = MyQueue()
+    queue = MyQueueWithStacks()
     queue.push(2)
     queue.push(3)
