414ce0f6ebabafbebcef8fb2a3bca26ef29aa64d
lenny <lennyweda@gmail.com>
2023-03-16 07:11:11 +0300

    (f/tree-questions): dfs and bfs applications

--- Data Structures/Tree-based-DSAs/Binary-tree/level-order-traversal.py
+++ Data Structures/Tree-based-DSAs/Binary-tree/level-order-traversal.py
@@ -34,9 +34,51 @@ def fill_level_order(root: Node, result, count = 0):
     fill_level_order(root.left, result, count)
     fill_level_order(root.right, result, count)
 
+
+""" space and time complexity:
+    space complexity is O(n)
+    time complexity is O(n)
+
+
+
+"""
+def bfs(root: Node):
+    if not root:
+        return []
+    result = []
+    queue = []
+
+    queue.append(root)
+    starting_queue_size = 1
+    sub_array = []
+    processed_nodes = 0
+
+    while queue:
+        currentNode = queue.pop(0)
+        sub_array.append(currentNode.val)
+        processed_nodes += 1
+
+        if currentNode.left:
+            queue.append(currentNode.left)
+           
+        if currentNode.right:
+            queue.append(currentNode.right) 
+           
+        if processed_nodes == starting_queue_size:
+            result.append(sub_array)
+            starting_queue_size = len(queue)
+            sub_array = []
+            processed_nodes = 0
+
+    return result
+
+
+
 if __name__ == "__main__":
   testTree = Node(3)
   testTree.left = Node(54)
   testTree.right = Node(5)
 
   print(levelOrder(testTree))
+
+  print(bfs(testTree))
---
+++ Data Structures/Tree-based-DSAs/Binary-tree/return-right-most-nodes.py
@@ -0,0 +1,29 @@
+"""
+    Imagine you are standing on the right of a binary tree, 
+    return the nodes you can see from top to bottom in an array
+
+
+"""
+
+
+class Node:
+    def __init__(self, val):
+        self.left = None
+        self.right = None
+        self.val = val
+
+def right_side_view(root):
+    result = []
+    fill_right_side_view(root, result)
+    return result
+
+def fill_right_side_view(root: Node, result, level = 0):
+    if root is None:
+        return
+    level += 1
+    if len(result) < level:
+        result.append(root.val)
+    else:
+        result[level - 1] = root.val
+    fill_right_side_view(root.left, result, level)
+    fill_right_side_view(root.right, result, level)
