0ea56106a4fe09945645fa5bddbd86b279618142
lenny <lennyweda@gmail.com>
2023-03-22 21:57:06 +0300

    (f/graph-questions): time to inform question done in python

--- Data Structures/Graph-based-DSAs/time-to-inform-employees.py
+++ Data Structures/Graph-based-DSAs/time-to-inform-employees.py
@@ -3,26 +3,31 @@
 
 
 """
+"""
+    Time complexity is : O(n)
+    space complexity is : O(n)
 
+
+"""
 def time_to_inform(n, headID, manager, informTime):
     graph = build_adjacency_list(n, manager)
     Maxtime = 0
-    dfs(headID, graph, informTime, Maxtime)
+    Maxtime = dfs(headID, graph, informTime)
     return Maxtime
 
 def build_adjacency_list(n, manager):
-    graph = []
+    graph = [[] for i in range(n)]
     for index in range(n):
-        if not graph[manager[index]]:
-            graph[manager[index]] = []
-        graph[manager[index]].append(index)
+        if manager[index] != -1:
+            graph[manager[index]].append(index)
     return graph
 
-def dfs(headID, graph, informTime, Maxtime):
+def dfs(headID, graph, informTime):
     if not graph[headID]:
-        return Maxtime
-    Maxtime += informTime[headID]
-    for heads in graph[headID]:
-        Maxtime = max(Maxtime, dfs(heads, graph, informTime, Maxtime))
-    return Maxtime
+        return 0
+    time = 0
+    for head in graph[headID]:
+        time = max(time, dfs(head, graph, informTime))
+    time += informTime[headID]
+    return time
 
