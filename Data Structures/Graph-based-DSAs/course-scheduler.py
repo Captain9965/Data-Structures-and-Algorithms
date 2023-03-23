8fd60eceb31fccdc6bdc269ac2fa6e23d995489c
lennyweda <lennyweda@gmail.com>
2023-03-23 17:13:30 +0300

    (f/graph-questions): course scheduler WIP

--- Data Structures/Graph-based-DSAs/course-scheduler.py
+++ Data Structures/Graph-based-DSAs/course-scheduler.py
@@ -7,3 +7,38 @@
     As a result, we use topological sort...which works on directed acyclic graph..
 
 """
+def canFinish(numCourses, prerequisites):
+    adjList = [[] for i in range(numCourses)]
+    inDegreeMatrix = [0 for i in range(numCourses)]
+    createLists(numCourses, prerequisites, adjList, inDegreeMatrix)
+    return not hasCycle(inDegreeMatrix, adjList)
+
+
+def createLists(numCourses, prerequisites, adjList, inDegreeMatrix):
+    for course in prerequisites:
+        adjList[course[1]].append(course[0])
+        inDegreeMatrix[course[0]] += 1
+
+def hasCycle(inDegreeMatrix, adjList):
+    seen = []
+    currentNode = None
+    while 1:
+        for i in range(len(inDegreeMatrix)):
+            if inDegreeMatrix[i] == 0 and i not in seen:
+                currentNode = i
+                break;
+        if currentNode is None:
+            break;
+        seen.append(currentNode)
+        for child in adjList[currentNode]:
+            inDegreeMatrix[child] -= 1
+    if len(seen) == len(inDegreeMatrix):
+        return True
+    else:
+        return False
+
+
+
+if __name__ == "__main__":
+    prerequisites = [[1,0],[0,1]]
+    canFinish(len(prerequisites), prerequisites)
