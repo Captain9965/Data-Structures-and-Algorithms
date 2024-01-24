b6b78f69aa76a33c6d76966b54e171fe9efe154a
lenny <lennyweda@gmail.com>
2024-01-24 07:03:48 +0300

    (master): minimum difference between any 2 values in a BST

---
+++ Data Structures/Tree-based-DSAs/Binary-tree/ minimum-difference.py
@@ -0,0 +1,22 @@
+"""
+Question:
+    Get the minimum difference between any 2 nodes in a BST..
+
+"""
+
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        def fn(node, lo, hi):
+            if not node: return hi - lo
+            left = fn(node.left, lo, node.val)
+            right = fn(node.right, node.val, hi)
+            return min(left, right)
+        return fn(root, float('-inf'), float('inf'))
